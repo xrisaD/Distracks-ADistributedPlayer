35d2907aff08f4c4a3b7e9a2d9577630a0854633
NickSmyr <nikos.1999@live.com>
2020-03-22 12:14:57 +0200

    Request reply objects

---
+++ Request.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+class Request {
+    class StatusCodes {
+        public static final int MALFORMED_REQUEST = 400;
+        public static final int NOT_FOUND = 404;
+
+        public static final int OK = 200;
+
+        public static final int NOT_RESPONSIBLE = 300;
+    }
+    static class RequestToBroker {
+
+        int method; //METHOD_PULL or METHOD_NOTIFY or METHOD_STATUS  or (METHOD_SEARCH)
+
+        //Fields for the notify method
+        ArrayList<String> artistNames = new ArrayList<>();
+        String publisherIp;
+        int publisherPort;
+
+        //Fields for the pull method
+        String pullArtistName;
+        String songName;
+
+        //Fields for the status method
+        //None
+
+        //Fields for the search method
+        String query;
+
+    }
+
+    static class ReplyFromBroker {
+
+        int method; //METHOD_PULL or METHOD_NOTIFY or METHOD_STATUS  or (METHOD_SEARCH)
+        int statusCode;
+        //Fields for the notify method
+        //Nonde
+
+        //Fields for the pull method
+        int numChunks;
+        String responsibleBrokerIp;
+        String responsibleBrokerPort;
+
+        //Fields for the status method
+        ArrayList<String> artists;
+
+        //Fields for the search method
+        ArrayList<String> searchArtists;
+        ArrayList<String> searchSongs;
+
+    }
+}
