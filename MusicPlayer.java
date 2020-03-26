ed974488601444ebb279092f604d0f1b565dd613
Nikos <smasidjaisd>
2020-03-26 22:04:24 +0200

    Music Player supports incomplete files , Incomplete List a synchronized data structure that supports a blocking for each iteration

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -12,11 +12,27 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils ;
 
 public class MusicPlayer extends Application {
+
+    private IncompleteList<MusicFile> list;
+    MediaPlayer mediaplayer;
+    public MusicPlayer(){
+
+    }
+    public MusicPlayer(IncompleteList<MusicFile> list){
+        this.list = list;
+        launch("test");
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
     public void start(Stage stage) {
         //Circle circ = new Circle(40, 40, 30);
         //Group root = new Group(circ);
@@ -33,34 +49,77 @@ public class MusicPlayer extends Application {
 
         System.out.println("Shiet\n");
         System.out.println("Getting song metadata");
-        List<MusicFileMetaData> songs = MP3Cutter.getSongsMetaData("a" , "z");
-        MusicFile mf = readFully(songs.get(10));
+        List<MusicFileMetaData> songs = new ArrayList<>();
+        songs.add(new MusicFileMetaData());
+        songs.get(0).setPath("untitled.mp3");
+        MusicFile mf = readFully(songs.get(0));
         //Play two songs at the same time
-        List<MusicFile> chunks = breakMusicFile(mf , 1024*512);
+        List<MusicFile> chunks = breakMusicFile(mf , 50000);
+        //System.out.println(chunks);
+
+        IncompleteList<MusicFile> nlist = new IncompleteList<>(chunks.size());
+        for(MusicFile m : chunks){
+            nlist.add(m);
+        }
         System.out.println("chunks : " + chunks.size());
-        playSequentially(chunks , 0);
-        //for(int i = 0 ; i < 1 ; i++){
-        //    play(chunks.get(i) , "tmp/chunk" + i + ".mp3" , () ->System.out.println("Done"));
-        //}
+        this.list = nlist;
+        this.play();
+
+    }
+    //METHODS
+    public void play(){
+        _playSequentially(list);
     }
 
     /**
-     * THIS IS THE HARDEST PIECE OF CODE I HAVE EVER WRITTEN
+     *  Outputs every musicFile as they become available (they come over the network) as a temp mp3 file
+     *  and outputs the filenames as medias for use by another thread
      */
-    public void playSequentially(List<MusicFile> musicFiles , int index){
-        if(musicFiles.isEmpty()) return;
-        play(musicFiles.get(0) , "tmp/chunk" + index +".mp3" , new Runnable(){
-            int i = 0;
+    private void _outputData(IncompleteList<MusicFile> musicFiles , IncompleteList<Media> filenames){
+        int i = 0;
+        for(MusicFile mf : musicFiles){
+            String filename = "tmp/chunk" + i + ".mp3";
+            createTempFile(filename);
+            musicFileToMp3(mf , filename);
+            Media m = new Media(new File(filename).toURI().toString());
+            filenames.add(m);
+            i++;
+            //System.out.println(musicFiles);
+            //System.err.println(filenames);
+        }
+    }
+
+    /**
+     * Starts playing medias as soon as they become available
+     */
+    private void _playMediaTracks(Iterator<Media> it) {
+        if (!it.hasNext()) {
+            System.err.println("Iterator doesn't have next");
+            return;
+        }
+        System.err.println("Asking for the next media");
+        mediaplayer = new MediaPlayer(it.next());
+        mediaplayer.setOnEndOfMedia(new Runnable() {
+            @Override
             public void run() {
-                //Rmoving the song that just played
-                musicFiles.remove(0);
-                System.out.println("Moving to the next lil chunk");
-                playSequentially(musicFiles, index+1);
-                i++;
+                System.out.println("Going to the next one / end of media");
+                _playMediaTracks(it);
             }
         });
-
+        mediaplayer.play();
     }
+
+    private void _playSequentially(IncompleteList<MusicFile> musicFiles){
+        //One thread will createThF
+        IncompleteList<Media> medias = new IncompleteList<>(musicFiles.numItems);
+        new Thread(()-> _outputData(musicFiles,medias)).start();
+        new Thread(()-> _playMediaTracks(medias.iterator())).start();
+    }
+
+
+    // IO HELPER FUNCTIONS
+
+
     public static List<MusicFile> breakMusicFile(MusicFile musicFile, int chunkSize){
         byte[] largeFile = musicFile.getMusicFileExtract();
         ArrayList<byte[]> extracts = new ArrayList<>();
@@ -72,7 +131,6 @@ public class MusicPlayer extends Application {
             if(largeFile.length - offset < chunkSize){
                 chunk = new byte[largeFile.length - offset];
             }
-            System.out.println(chunk.length);
             for(int i = 0 ; i < chunk.length ; i++){
                 chunk[i] = largeFile[offset];
                 offset++;
@@ -125,7 +183,7 @@ public class MusicPlayer extends Application {
         try{
             tmpFile.delete();
             tmpFile.createNewFile();
-            //tmpFile.deleteOnExit();
+            tmpFile.deleteOnExit();
         }
         catch (Exception e){
             e.printStackTrace();
